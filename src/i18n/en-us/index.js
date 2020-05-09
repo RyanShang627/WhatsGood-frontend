61eb747285d053288834a51f90dcb96af3f86f3c
RyanShang627 <dadong.shang@outlook.com>
2020-05-08 18:37:55 -0700

    feat: Add new keywords

--- src/i18n/en-us/index.js
+++ src/i18n/en-us/index.js
@@ -1,7 +1,7 @@
-// This is just an example,
-// so you can safely delete all default props below
-
 export default {
-  failed: 'Action failed',
-  success: 'Action was successful'
-}
+  failed: "Action failed",
+  success: "Action was successful",
+  Profile: "Profile",
+  Support: "Support",
+  Logout: "Logout"
+};
